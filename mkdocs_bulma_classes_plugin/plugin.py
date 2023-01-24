70d371be005bbd5c3d0480a9b362c3c1dff7cc4b
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2023-01-24 11:11:54 +0100

    fix: don't overwrite the table class in table elements

--- mkdocs_bulma_classes_plugin/plugin.py
+++ mkdocs_bulma_classes_plugin/plugin.py
@@ -12,11 +12,11 @@ class BulmaClassesPlugin(BasePlugin):
     }
 
     markdown_regexes = {
-        "(.*)\n(=+)(\n+)(.*)\n(-+)": '<h1 class="title is-1 has-text-light" id="\g<1>">\g<1></h1><h3 class="subtitle is-3 has-text-light" id="\g<4>">\g<4></h3>'
+        "(.*)\n(=+)(\n+)(.*)\n(-+)": '<h1 class="title is-1 has-text-light" id="\g<1>">\g<1></h1><h3 class="subtitle is-3 has-text-light" id="\g<4>">\g<4></h3>',
     }
 
     regex_dict = {
-        "<table>": '<table class="table">',
+        '<table (id="\w*")? class="((?!table).*)">': '<table \g<1> class="table \g<2>">', # Look at https://regex101.com/r/3UZHgz/1 for unit tests
         '<h1 id="([\w-]*)">': '<h1 id="\g<1>" class="title is-1 has-text-light">',
         '<h2 id="([\w-]*)">': '<h2 id="\g<1>" class="title is-2 has-text-light">',
         '<h3 id="([\w-]*)">': '<h3 id="\g<1>" class="title is-3 has-text-light">',
